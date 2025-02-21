42746e278e0c435a912b857c19f3b9d949ca95ef
Chaymae Mechouahi <chaymaemechouahi@gmail.com>
2025-02-21 12:54:43 +0100

    CMI:Checker si la logique métier marche
    (Valide avec des petites modifs à faire)

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,21 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sales/view/auth.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(); // Initialisation de Firebase
+
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyAWjemoe2wf3SI0Fp-CtK64NQeSkD6meqc", 
+      appId: "1:115362818316:web:b07edf5a663191af4ac129",
+      messagingSenderId: "115362818316", 
+      projectId: "sales-797d9", 
+      authDomain: "sales-797d9.firebaseapp.com", 
+      storageBucket: "sales-797d9.firebasestorage.app", 
+    ),
+  );
+
+  // Lancez l'application
   runApp(MyApp());
 }
 
@@ -14,7 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Firebase Auth',
-      home: AuthPage(),
+      home: AuthPage(), 
     );
   }
 }
